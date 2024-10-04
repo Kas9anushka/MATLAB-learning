4d1818aa07c09ee3f70456ed21b00d6d8ccd637d
Kumari Anushka Singh <145387862+Kas9anushka@users.noreply.github.com>
2024-10-04 23:23:03 +0530

    Add files via upload

---
+++ basic proplems/creating_array.m
@@ -0,0 +1,14 @@
+% creating array
+
+% creating an array with equal spacing
+
+arr= [1:2:5]  % {first term : spacing : last term]
+arr1 =[1:5] % [first term : spacing =1 : last term]
+arr2=linspace(2,10,6) % [1st term, last term, no. of terms]
+zr=zeros(3,7) % create matrix 3*7 with 0 value
+on=ones(3,7)  % create matrix 3*7 with 1 value
+e=eye(7)      % square identity matrix 7*7
+arr1t=arr1'   % create transpose of matrix arr1
+v= arr1(3)    % store the value of an array v=3
+mat=[1 2 3;2 5 9;6 5 3]  % creating a matrix(3*3)
+m=mat(3,1)  % access the mat value at (3,1) =6
